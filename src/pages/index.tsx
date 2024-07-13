081688642b176f64e0e96fb60eef13b17ef2ac28
Davide Albano <davidealbano96@gmail.com>
2024-07-13 14:49:12 +0200

    added identity widget sitewide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import netlifyIdentity from "netlify-identity-widget"
 
 const pageStyles = {
   color: "#232129",
@@ -137,6 +138,10 @@ const links = [
 ]
 
 const IndexPage: React.FC<PageProps> = () => {
+  React.useEffect(() => {
+    netlifyIdentity.init()
+  }, [])
+
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
